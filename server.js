a9bb7b9ce6721b80262ac9d8114fee7c4f42f6c9
Justin Phung <justin.phung@mail.utoronto.ca>
2020-11-26 09:57:55 -0500

    Updated Server Routes

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 /* server.js - Express server*/
 'use strict';
 const log = console.log
-log('Express server')
 
 const express = require('express')
 const app = express();
-
 const path = require('path');
 
 // Setting up a static directory for the files in /pub
@@ -26,28 +24,7 @@ app.get('/', (req, res) => {
 	//res.send('This should be the root route!')
 
 	//sending some HTML
-	res.send('<h1>This should be the root route!</h1>')
-})
-
-// Error codes
-app.get('/problem', (req, res) => {
-	// You can indicate a status code to send back
-	// by default it is 200, but it's up to you
-	// if you want to send something
-	res.status(500).send('There was a problem on the server')
-
-	// don't send nonsense status codes like this one:
-	//res.status(867).send('There was a problem on the server')
-})
-
-// Sending some JSON
-app.get('/someJSON', (req, res) => {
-	// object converted to JSON string
-	res.send({
-		name: 'John',
-		year: 3,
-		courses: ['csc309', 'csc301']
-	})
+	res.sendFile(path.join(__dirname + '/pub/examples.html'))
 })
 
 // will use an 'environmental variable', process.env.PORT, for deployment.
